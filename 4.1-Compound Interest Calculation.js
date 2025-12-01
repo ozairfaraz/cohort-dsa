68ac960f8e13b5f6f8f964f3849e64f7be4e56c9
Faraz Ozair <farazozair5@gmail.com>
2025-12-01 23:45:46 +0530

    done

---
+++ 4.1-Compound Interest Calculation.js
@@ -0,0 +1,6 @@
+// function calculateCompoundInterest(P, r, t, n) {
+//     const A = P*(Math.pow(1+r/n,n*t));
+//     const Ci =A-P;
+//     return(Ci.toFixed(2));
+// }
+// module.exports = { calculateCompoundInterest };
