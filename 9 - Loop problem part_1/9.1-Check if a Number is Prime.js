de3754da2cef4e11ef8fd428949ac7286e8d96a2
Faraz Ozair <farazozair5@gmail.com>
2025-12-14 23:27:41 +0530

    done

--- 9 - Loop problem part_1/9.1-Check if a Number is Prime.js
+++ 9 - Loop problem part_1/9.1-Check if a Number is Prime.js
@@ -1,11 +1,12 @@
 function isPrime(n) {
     // Write your logic here
-    let flg=0;
-    for(let i=1; i<=n/2; i++){
-        if (n%i==0) flg++;
+    if (n <= 1) return "Not Prime";
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            return "Not Prime";
+        }
     }
-    if (flg===1) return "Prime";
-    else return "Not Prime";
+    return "Prime";
     
 }
 
