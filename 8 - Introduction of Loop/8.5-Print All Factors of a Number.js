a0ad20aff60577533ae3131780a032facc4a81fc
Faraz Ozair <farazozair5@gmail.com>
2025-12-14 22:45:09 +0530

    done

--- 8 - Introduction of Loop/8.5-Print All Factors of a Number.js
+++ 8 - Introduction of Loop/8.5-Print All Factors of a Number.js
@@ -1,9 +1,9 @@
 function printFactors(n) {
     let str = "";
-    for (let i = 1; i <= n; i++) {
+    for (let i = 1; i <= n/2; i++) {
         if (n % i === 0) str += i + " ";
     }
-    process.stdout.write(str);
+    process.stdout.write(str+n.toString());
 }
 
 module.exports = { printFactors };
